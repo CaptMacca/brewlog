0030569dacb3ee470ad3122156de2c462a7e1beb
CaptMacca <stev_mcdonald@hotmail.com>
2019-08-26 19:22:42 +0800

    Add method to validate token expiry

--- brew-ui/src/app/auth/services/auth-guard.service.ts
+++ brew-ui/src/app/auth/services/auth-guard.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 
 import { Store } from '@ngxs/store';
-import { AuthState } from '../state/auth.state';
+import { AuthState } from '@app/auth/state/auth.state';
+import { AuthService } from '@app/auth/services/auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router, private store: Store) {}
+  constructor(private router: Router, private store: Store, private authService: AuthService) {}
 
   canActivate(): boolean {
-    const isLoggedIn = this.store.selectSnapshot(AuthState.isLoggedIn);
-    if (!isLoggedIn) {
+    const token: string = this.store.selectSnapshot(AuthState.getAccessToken);
+    if (!this.authService.isAuthenticated(token)) {
       this.router.navigate(['user/login']);
       return false;
     }
--- brew-ui/src/app/auth/services/auth-interceptor.service.ts
+++ brew-ui/src/app/auth/services/auth-interceptor.service.ts
@@ -12,15 +12,15 @@ export class AuthInterceptorService implements HttpInterceptor {
     constructor(private store: Store) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        let authReq = req;
-        const token = this.store.selectSnapshot(AuthState.getAccessToken);
-        if (token != null) {
-            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-        }
-        return next.handle(authReq);
+      let authReq = req;
+      const token = this.store.selectSnapshot(AuthState.getAccessToken);
+      if (token) {
+        authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      }
+      return next.handle(authReq);
     }
 }
 
 export const httpInterceptorProviders = [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
 ];
