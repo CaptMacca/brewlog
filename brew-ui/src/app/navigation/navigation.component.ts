1e4627fc1332ed3366655c4a82fc3e2fffd1496c
Stephen McDonald <mackysm@gmail.com>
2020-06-21 12:34:46 +0800

    Correct nav-logo height

--- brew-ui/src/app/navigation/navigation.component.ts
+++ brew-ui/src/app/navigation/navigation.component.ts
@@ -5,7 +5,8 @@ import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
-  templateUrl: './navigation.component.html'
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit, OnDestroy {
 
